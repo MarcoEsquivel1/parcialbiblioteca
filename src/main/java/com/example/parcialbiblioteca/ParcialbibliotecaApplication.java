1f089b241c2333d643e736bfc8bee67c16310706
MarcoEsquivel <marco.esquivel@catolica.edu.sv>
2023-06-05 11:05:14 -0600

    setup

---
+++ src/main/java/com/example/parcialbiblioteca/ParcialbibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.example.parcialbiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialbibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialbibliotecaApplication.class, args);
+	}
+
+}
