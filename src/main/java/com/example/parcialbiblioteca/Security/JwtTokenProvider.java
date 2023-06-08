ea67755a98d95dafd0aea3c3ec5d55f7f64daac8
Julio aguilar <julioa.m777@hotmail.com>
2023-06-08 09:59:57 -0600

    feat: add payload to JWT

--- src/main/java/com/example/parcialbiblioteca/Security/JwtTokenProvider.java
+++ src/main/java/com/example/parcialbiblioteca/Security/JwtTokenProvider.java
@@ -1,11 +1,17 @@
 package com.example.parcialbiblioteca.Security;
 
+import com.example.parcialbiblioteca.entity.Usuario;
+import com.example.parcialbiblioteca.repository.UsuarioRepository;
 import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtTokenProvider {
@@ -16,15 +22,27 @@ public class JwtTokenProvider {
     @Value("${app.jwt-expiration-in-ms}")
     private int jwtExpiration;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     public String generateToken(Authentication authentication) {
         String username = authentication.getName();
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + jwtExpiration);
 
+        Usuario usuario = usuarioRepository.findByEmail(username).get();
+
+        Map<String, Object> usuarioData = new HashMap<>();
+        usuarioData.put("name", usuario.getNombre());
+        usuarioData.put("lastName", usuario.getApellido());
+
+
         String token = Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(expiryDate)
+                .claim("Data", usuarioData)
+                .claim("Roles", authentication.getAuthorities())
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
 
