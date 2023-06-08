7e0f24f655ab353568160d577138d0a77a0d3c57
Galdámez <galdamez.alberto@outlook.com>
2023-06-08 15:15:31 -0600

    feat: global cors filter added

--- src/main/java/com/example/parcialbiblioteca/configuration/SecurityConfig.java
+++ src/main/java/com/example/parcialbiblioteca/configuration/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.example.parcialbiblioteca.configuration;
 import com.example.parcialbiblioteca.Security.CustomUserDetailsService;
 import com.example.parcialbiblioteca.Security.JwtAuthenticationEntryPoint;
 import com.example.parcialbiblioteca.Security.JwtAuthenticationFilter;
+
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +22,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -41,33 +47,23 @@ public class SecurityConfig {
                 .passwordEncoder(passwordEncoder());
     }
 
-//    @Bean
-//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .exceptionHandling()
-//                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                .requestMatchers(HttpMethod.GET, "/api/**")
-//                .permitAll()
-//                .requestMatchers("api/auth/**")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated();
-//        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-//        return http.build();
-//    }
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
                 .permitAll();
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
