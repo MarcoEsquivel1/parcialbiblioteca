f12120d640f6544fec20a9e43d57197c0892901d
Mario Ernesto Carranza <mario.carranza996@gmail.com>
2023-06-08 14:51:44 -0600

    esta mierda buena

--- src/main/java/com/example/parcialbiblioteca/controller/UsuarioController.java
+++ src/main/java/com/example/parcialbiblioteca/controller/UsuarioController.java
@@ -8,8 +8,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(allowedHeaders = "Authorization")
 @RequestMapping("/api/usuarios")
+
 public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
